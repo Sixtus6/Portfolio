b49f7e39125cd97f88c126379deda1713da50e85
sixtus6 <xystuseton@gmail.com>
2023-12-04 09:15:43 +0100

    completed screens

--- lib/view/main/components/drawer/knowledges.dart
+++ lib/view/main/components/drawer/knowledges.dart
@@ -13,13 +13,17 @@ class Knowledges extends StatelessWidget {
         Divider(),
         Padding(
           padding: EdgeInsets.symmetric(vertical: 10),
-          child: Text('Knowledge',style: TextStyle(color: Colors.white),),
+          child: Text(
+            'Knowledge',
+            style: TextStyle(color: Colors.white),
+          ),
         ),
         KnowledgeText(knowledge: 'Flutter, Dart'),
-        KnowledgeText(knowledge: 'Networking, Cyber Security'),
+        KnowledgeText(knowledge: 'Ethical Hacking, Cyber Security'),
         KnowledgeText(knowledge: 'Git, Github'),
+        KnowledgeText(knowledge: 'Web3, Blockchain'),
+        KnowledgeText(knowledge: 'Node, Javascript'),
       ],
     );
   }
-
 }
